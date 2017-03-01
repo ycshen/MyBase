3af9415df4e1d2582ca4f843d5b52e0b05d8cc3c
shenyuchuan <shenyuchuan@itiaolingcom>
2017-03-01 17:38:31 +0800

    add impl

--- src/main/java/com/brp/service/impl/RoleUserServiceImpl.java
+++ src/main/java/com/brp/service/impl/RoleUserServiceImpl.java
@@ -1,58 +1,39 @@
 package com.brp.service.impl;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.brp.entity.AuthorityEntity;
-import com.brp.entity.AuthorityUserEntity;
 import com.brp.entity.RoleUserEntity;
-import com.brp.entity.UserEntity;
-import com.brp.mapper.AuthorityMapper;
-import com.brp.mapper.AuthorityUserMapper;
-import com.brp.mapper.UserMapper;
-import com.brp.service.AuthorityService;
-import com.brp.service.AuthorityUserService;
+import com.brp.mapper.RoleUserMapper;
 import com.brp.service.RoleUserService;
-import com.brp.util.query.AuthorityQuery;
-import com.brp.util.query.AuthorityUserQuery;
-import com.brp.util.query.AuthorityVOQuery;
-import com.brp.util.vo.AuthorityVO;
 
 /** 
  * <p>Project: MyBase</p> 
- * <p>Title: AuthorityServiceImpl.java</p> 
+ * <p>Title: RoleServiceImpl.java</p> 
  * <p>Description: TODO</p> 
  * <p>Copyright (c) 2016 xjw Consultancy Services</p>
  * <p>All Rights Reserved.</p>
- * @author <a href="mailto:shenyuchuan@itiaoling.com">申鱼川</a>
+ * @roleor <a href="mailto:shenyuchuan@itiaoling.com">申鱼川</a>
  */
 @Service
 public class RoleUserServiceImpl implements RoleUserService{
-
+	@Autowired
+	private RoleUserMapper roleUserMapper;
 	@Override
 	public void cancelRole(String idList, String companyId) {
-		// TODO Auto-generated method stub
-		
+		roleUserMapper.cancelRole(idList, companyId);
 	}
 
 	@Override
 	public void insertRoleUser(RoleUserEntity roleUser) {
-		// TODO Auto-generated method stub
-		
+		roleUserMapper.insertRoleUser(roleUser);	
 	}
 
 	@Override
 	public void batchCancelRole(String userIdArr, String companyId,
 			String roleId) {
-		// TODO Auto-generated method stub
-		
+		roleUserMapper.batchCancelRole(userIdArr, companyId, roleId);
 	}
-	
 	
 }
 
