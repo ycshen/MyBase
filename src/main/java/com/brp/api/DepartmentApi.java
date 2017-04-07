573314d08908ce6bf9a62848ec1684c35ad46a05
ycshen <1966225457@qq.com>
2017-04-07 17:12:06 +0800

    修改部门

--- src/main/java/com/brp/api/DepartmentApi.java
+++ src/main/java/com/brp/api/DepartmentApi.java
@@ -97,7 +97,6 @@ public class DepartmentApi {
 	}
 
 
-
 	@RequestMapping(value = "/getAllDepByCompanyId", method = RequestMethod.POST)
 	@ResponseBody
 	public String getAllDepByCompanyId(@RequestBody JSONObject jsonObject){
